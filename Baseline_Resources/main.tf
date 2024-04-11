0677f6972c6bbef6f9c7b6b7e259cae39fc01597
mjinga01 <memoryjinga@gmail.com>
2024-04-11 16:05:56 -0400

    Test 01

---
+++ Baseline_Resources/main.tf
@@ -0,0 +1,21 @@
+# resource "azurerm_resource_group" "test" {
+#   name   = var.rg_name
+#   location = var.region
+#   tags = {
+#     environment = var.environment
+#     owner       = var.owner
+#   }
+# }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "logstorage01"
+  resource_group_name      = var.rg_name
+  location                 = var.region
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = var.environment
+
+  }
+}
---
+++ Baseline_Resources/variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
