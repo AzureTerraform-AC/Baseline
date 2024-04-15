354eaab263aefff480445716c48cf9c2b1eb48ec
mjinga01 <memoryjinga@gmail.com>
2024-04-15 14:36:17 -0400

    Updated files

---
+++ main.tf
@@ -0,0 +1,10 @@
+module "baseline" {
+    source = "./modules"
+
+    prefix = var.prefix
+    region = var.region
+    environment = var.environment
+    owner = var.owner
+    rg_name = var.rg_name
+    
+}
