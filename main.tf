8f52ab3cba1e8a23f381fdef8c3b52ad03bbf092
Franklin <addofrancium@gmail.com>
2024-04-19 14:43:10 -0400

    adding providers to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
 module "baseline" {
   source = "./modules/Baseline_Resources"
 
